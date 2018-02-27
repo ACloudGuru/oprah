abd9409bb72553ca1b7627ae6dfca004491b5559
Zac Lang <zac.lang@acloud.guru>
2018-02-27 17:25:46 +1100

    fix vscode prettier extension config resolution

--- packages/acg-scripts/frontend/scripts/format.js
+++ packages/acg-scripts/frontend/scripts/format.js
@@ -1,18 +1,13 @@
 'use strict';
 
-const path = require('path');
 const spawn = require('cross-spawn');
 const resolveBin = require('../../utils/resolveBin').resolveBin;
 
-const configPath = path.join(__dirname, '../..', 'config', '.prettierrc.yml');
-
-const config = ['--config', configPath, '--write'];
+const config = ['--config', 'prettierrc.config.js', '--write'];
 const args = process.argv.slice(2);
 
-const result = spawn.sync(
-  resolveBin('prettier'),
-  [...config, ...args],
-  { stdio: 'inherit' }
-);
+const result = spawn.sync(resolveBin('prettier'), [...config, ...args], {
+  stdio: 'inherit',
+});
 
 process.exitCode = result.status;
