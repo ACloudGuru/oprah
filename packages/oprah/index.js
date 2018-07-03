0750c559a8e7fe4e1ffe066b849d3d0bcb915db5
chronotc <chronotc@users.noreply.github.com>
2018-07-03 13:41:11 +1000

    remove validation

--- packages/oprah/index.js
+++ packages/oprah/index.js
@@ -1,15 +1,10 @@
 'use strict';
-
-const { oprah } = require('./lib/oprah');
-const { validateArguments } = require('./lib/validate-arguments');
 const { populateConfig } = require('./lib/populate-config');
 const { populateSecret } = require('./lib/populate-secret');
 const { readCfOutputs } = require('./lib/read-cf-outputs');
 
 
 module.exports = {
-  oprah,
-  validateArguments,
   populateConfig,
   populateSecret,
   readCfOutputs
